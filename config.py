437351ded5c700f80f3a314d4aef29fa35da0925
kane5 <Nguyenkhactam5@gmail.com>
2025-02-05 00:52:54 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 from os import environ
 
-API_HASH = environ.get("API_HASH", "72cc7062951f56abb5240994690e49de")
-API_ID = int(environ.get("API_ID", "25811007"))
-BOT_TOKEN = environ.get("BOT_TOKEN", "7789014828:AAF_85sQOhwProRoVKGuqidjFAlHCQZtUCo")
-BOT_OWNER = int(environ.get("BOT_OWNER", "7240555847"))
-BOT_USERNAME = environ.get("BOT_USERNAME", "tymchomapbot")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002465408399"))
-AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1002271972643"))
+API_HASH = environ.get("API_HASH", "146fcf83a1670e2e926ce874ce31fcb3")
+API_ID = int(environ.get("API_ID", "29762406"))
+BOT_TOKEN = environ.get("BOT_TOKEN", "7604302137:AAG_sUd_5QS4Ti10b3z76hlLfvh5mzGMu6A")
+BOT_OWNER = int(environ.get("BOT_OWNER", "6670233911"))
+BOT_USERNAME = environ.get("BOT_USERNAME", "MatXaMuongThanh_BOT")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002489615288"))
+AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1002255138114"))
 DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://nguyenkhactam5:q1231234@kanereaction.zhqqd.mongodb.net/?retryWrites=true&w=majority&appName=kanereaction")
 
 # Define default emojis list
@@ -16,5 +16,5 @@ EMOJIS = [
     "❤‍🔥", "🌚", "😐", "💯", "🦄", "⚡", "👾", "🏆", "💔", "🤨", "🌟", "😡", 
     "👅", "🆒", "😘", "😈", "😴", "😭", "👻", "🌈", "👨‍💻", "👀", "🎃", "🙄", 
     "🥇", "🎉", "🕊️", "🆑", "🥳", "🏧", "🚀", "😛", "😻", "🕊️", "🦸", "💫",
-    "🤧", "😨", "🤝", "🤐", "🤗", "🫡", "🤭", "🥸", "🤫", "😶‍🌫", "🤪", "😏",
+    "🤧", "😨", "🤝", "🤐", "🤗", "🤖", "🤭", "👻", "🤫", "😶‍🌫", "🤪", "😏",
 ]
